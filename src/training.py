1006c5033a3adfee38bd13112ab8be94cd87e504
lpsilvestrin <luisp.silvestrin@gmail.com>
2017-02-10 16:17:51 +0100

    corrected data parse

--- src/training.py
+++ src/training.py
@@ -9,18 +9,18 @@ Ytr = np.genfromtxt('../data/Ytr.csv', delimiter=',',skip_header=1)
 X=Xtr[:,0:-1]
 X_train=X[0:4000,:]
 X_test=X[4000:5000,:]
-Y_train=Ytr[0:4000,1]
-Y_test=Ytr[4000:5000,1]
+Y = Ytr[:,1]
 
+indptr = range(len(Y)+1)
+Y_matrix= np.zeros((len(Y),10))
+for i in xrange(len(Y)):
+    Y_matrix[i][int(Y[i])]=1
 
-indptr = range(len(Y_train)+1)
-Y_matrix= np.zeros((len(Y_train),10))
-for i in xrange(len(Y_train)):
-    Y_matrix[i][int(Y_train[i])]=1
+Y_train = Y_matrix[0:4000]
+Y_test = Y_matrix[4000:5000]
 
 topology = [3072, 200, 10]
-X_train
-Y=Y_train
+
 nn = Neural_Network_modular(topology, 0.1, X_train[0], Y_train)
 batch_size = 20
 nn.batch_train(X_train, Y_train, batch_size)
